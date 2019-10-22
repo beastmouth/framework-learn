bd792eccb35bb2f6f6131236b72c92e92ba78203
hbj <hbj@33.cn>
2019-10-22 17:44:24 +0800

    SpringBean的生命周期

--- src/main/java/com/narcos/frameworklearn/FrameworkLearnApplication.java
+++ src/main/java/com/narcos/frameworklearn/FrameworkLearnApplication.java
@@ -1,11 +1,6 @@
 package com.narcos.frameworklearn;
 
-import com.narcos.frameworklearn.bean.BeanOne;
-import com.narcos.frameworklearn.bean.BeanTwo;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.config.BeanDefinition;
-import org.springframework.beans.factory.support.BeanDefinitionBuilder;
-import org.springframework.beans.factory.support.DefaultListableBeanFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -26,30 +21,5 @@ public class FrameworkLearnApplication {
 //        Person person = (Person) applicationContext.getBean("person");
 //        System.out.println("personBean : " + personBean.toString() + " personName : " + person.toString());
 //        person.call();
-
-        DefaultListableBeanFactory beanFactory = (DefaultListableBeanFactory) applicationContext.getAutowireCapableBeanFactory();
-        BeanDefinitionBuilder beanDefinitionBuilder = BeanDefinitionBuilder.genericBeanDefinition(BeanOne.class);
-        beanDefinitionBuilder.addConstructorArgValue("I am BeanOne");
-        beanDefinitionBuilder.setInitMethodName("init");
-        beanDefinitionBuilder.setDestroyMethodName("destroy");
-        BeanDefinition interceptorBeanDefinition = beanDefinitionBuilder
-                .getRawBeanDefinition();
-        beanFactory.registerBeanDefinition("beanOne",
-                interceptorBeanDefinition);
-        BeanOne beanOne = (BeanOne) applicationContext.getBean("beanOne");
-        log.info(beanOne.getWords());
-        beanOne.destroy();
-
-        BeanDefinitionBuilder beanDefinitionBuilder2 = BeanDefinitionBuilder.genericBeanDefinition(BeanTwo.class);
-        beanDefinitionBuilder2.addConstructorArgValue("I am BeanTwo");
-        beanDefinitionBuilder2.setInitMethodName("init");
-        beanDefinitionBuilder2.setDestroyMethodName("destroy");
-        BeanDefinition interceptorBeanDefinition2 = beanDefinitionBuilder2
-                .getRawBeanDefinition();
-        beanFactory.registerBeanDefinition("beanTwo",
-                interceptorBeanDefinition2);
-        BeanTwo beanTwo = (BeanTwo) applicationContext.getBean("beanTwo");
-        log.info(beanTwo.getWords());
-        beanTwo.destroy();
     }
 }
